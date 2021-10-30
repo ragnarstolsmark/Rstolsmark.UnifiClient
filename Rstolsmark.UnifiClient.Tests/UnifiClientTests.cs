98a6fdf59a3cc87b49707dbec63b6006ce3d101a
Ragnar Stølsmark <rstolsmark@live.no>
2021-10-30 10:32:55 +0200

    Inlined JWT token

--- Rstolsmark.UnifiClient.Tests/UnifiClientTests.cs
+++ Rstolsmark.UnifiClient.Tests/UnifiClientTests.cs
@@ -12,6 +12,7 @@ namespace Rstolsmark.UnifiClient.Tests
     {
         UnifiClient _unifiClient;
         TestClock _testClock;
+        private string _jwtToken;
 
         public UnifiClientTests()
         {
@@ -35,6 +36,10 @@ namespace Rstolsmark.UnifiClient.Tests
                 }
             };
             _unifiClient = new UnifiClient(cache, options);
+            //The jwt token is valid to 11.10.2021 15:33:28 UTC
+            //It also contains a clam named csrfToken that is valid in the same time span
+            _jwtToken =
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjc3JmVG9rZW4iOiJkN2ExNmNkOS1mNzljLTQwYjgtYjllMi1lNTA0MGI5ZmQ4YjEiLCJ1c2VySWQiOiIyZTJiMTNjNC05YjZkLTRkYWMtYmVhMS04OTA4OGU3ODgwMjciLCJpYXQiOjE2MzM5NjI4MDgsImV4cCI6MTYzMzk2NjQwOH0.uOQhY_VONOCjFvTIggagoPE4uL6Rbk0ByqAMDKzdJGw";
         }
         [Fact]
         public async Task Login_Should_Return_False_On_Failure()
@@ -47,11 +52,9 @@ namespace Rstolsmark.UnifiClient.Tests
         public async Task Login_Should_Return_True_On_Success()
         {
             using var httpTest = new HttpTest();
-            //jwttokenet er valid to 11.10.2021 15:33:28 UTC
-            //inneholder også et claim som er csrfToken som er gyldig like lenge
-            var jwtToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjc3JmVG9rZW4iOiJkN2ExNmNkOS1mNzljLTQwYjgtYjllMi1lNTA0MGI5ZmQ4YjEiLCJ1c2VySWQiOiIyZTJiMTNjNC05YjZkLTRkYWMtYmVhMS04OTA4OGU3ODgwMjciLCJpYXQiOjE2MzM5NjI4MDgsImV4cCI6MTYzMzk2NjQwOH0.uOQhY_VONOCjFvTIggagoPE4uL6Rbk0ByqAMDKzdJGw";
+            //the cookie is called TOKEN
             httpTest
-                .RespondWith(status: 200, cookies: new { TOKEN = jwtToken }); //the cookie is called TOKEN
+                .RespondWith(status: 200, cookies: new { TOKEN = _jwtToken }); 
             Assert.True(await _unifiClient.Login());
         }
     }
