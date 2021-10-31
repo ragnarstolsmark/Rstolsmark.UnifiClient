81b3b02230904d19ecaa5863e1eb8c34f8a33242
Ragnar Stølsmark <rstolsmark@live.no>
2021-10-31 07:18:37 +0100

    Added test of GetPortForwardSettings

--- Rstolsmark.UnifiClient.Tests/UnifiClientTests.cs
+++ Rstolsmark.UnifiClient.Tests/UnifiClientTests.cs
@@ -1,17 +1,20 @@
 using System;
+using System.IO;
 using System.Threading.Tasks;
 using Xunit;
 using Flurl.Http.Testing;
 using Microsoft.Extensions.Caching.Memory;
+using NuGet.Frameworks;
 
 namespace Rstolsmark.UnifiClient.Tests
 {
     public class UnifiClientTests
     {
-        UnifiClient _unifiClient;
-        TestClock _testClock;
-        private string _jwtToken;
-        private UnifiClientOptions _options;
+        private readonly UnifiClient _unifiClient;
+        private readonly TestClock _testClock;
+        private readonly string _jwtToken;
+        private readonly UnifiClientOptions _options;
+        private const string ResponseFolder = "responses";
         public UnifiClientTests()
         {
             var loginDate = new DateTimeOffset(2021, 10, 11, 14, 33, 0, 0, TimeSpan.Zero);
@@ -36,8 +39,7 @@ namespace Rstolsmark.UnifiClient.Tests
             _unifiClient = new UnifiClient(cache, _options);
             //The jwt token is valid to 11.10.2021 15:33:28 UTC
             //It also contains a clam named csrfToken that is valid in the same time span
-            _jwtToken =
-                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjc3JmVG9rZW4iOiJkN2ExNmNkOS1mNzljLTQwYjgtYjllMi1lNTA0MGI5ZmQ4YjEiLCJ1c2VySWQiOiIyZTJiMTNjNC05YjZkLTRkYWMtYmVhMS04OTA4OGU3ODgwMjciLCJpYXQiOjE2MzM5NjI4MDgsImV4cCI6MTYzMzk2NjQwOH0.uOQhY_VONOCjFvTIggagoPE4uL6Rbk0ByqAMDKzdJGw";
+            _jwtToken = File.ReadAllText(Path.Combine(ResponseFolder,"jwtToken.txt"));
         }
         [Fact]
         public async Task Login_Should_Throw_Exception_On_Failure()
@@ -47,12 +49,10 @@ namespace Rstolsmark.UnifiClient.Tests
             await Assert.ThrowsAsync<LoginException>( _unifiClient.Login);
         }
         [Fact]
-        public async Task GetTokens_Should_Cache_Tokens()
+        public async Task Get_Tokens_Should_Cache_Tokens()
         {
             using var httpTest = new HttpTest();
-            //the cookie is called TOKEN
-            httpTest
-                .RespondWith(status: 200, cookies: new { TOKEN = _jwtToken }); 
+            AddLoginSuccessCall(httpTest);
             await _unifiClient.GetTokens();
             await _unifiClient.GetTokens();
             //Forward the test clock to two minutes after the token expires to see that login is called again
@@ -62,7 +62,27 @@ namespace Rstolsmark.UnifiClient.Tests
                 .WithContentType("application/json")
                 .WithRequestBody($@"{{""username"":""{_options.Credentials.Username}"",""password"":""{_options.Credentials.Password}""}}")
                 .Times(2);
-                
+        }
+
+        private void AddLoginSuccessCall(HttpTest httpTest)
+        {
+            //the cookie is called TOKEN
+            httpTest
+                .RespondWith(cookies: new {TOKEN = _jwtToken});
+        }
+
+        [Fact]
+        public async Task Get_Port_Forward_Settings_Should_Return_List()
+        {
+            using var httpTest = new HttpTest();
+            AddLoginSuccessCall(httpTest);
+            var portForwardResponse =
+                await File.ReadAllTextAsync(Path.Combine(ResponseFolder, "GetCurrentPortForward.json")); 
+            httpTest
+                .RespondWith(portForwardResponse);
+            var portForwardSettings = await _unifiClient.GetPortForwardSettings();
+            Assert.Single(portForwardSettings);
+            Assert.Equal("57.173.50.35",portForwardSettings[0].Source);
         }
     }
 }
